ded68cea63ace310804b945acffca01e74b155ef
Valerio Volpe <kyuubibyte@live.it>
2020-02-20 11:48:00 +0100

    New Project

---
+++ StreetARtist/StreetARtist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StreetARtist
+//
+//  Created by Valerio Volpe on 20/02/2020.
+//  Copyright © 2020 Valerio Volpe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
